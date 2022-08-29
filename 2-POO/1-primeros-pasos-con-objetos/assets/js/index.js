7907b417e99decb6a2ce73fa9714cd034d507031
Laura Camila Quimbaya <camila111paco@gmail.com>
2022-08-29 18:28:01 -0500

    poo

---
+++ 2-POO/1-primeros-pasos-con-objetos/assets/js/index.js
@@ -0,0 +1,19 @@
+let usuario = {
+    nombre: 'user',
+    apellidos: 'my user',
+    email: 'user@user.com',
+    contraseña: 'pass92',
+    roles: [
+        {
+            role: 'admin'
+        },
+        {
+            role: 'user'
+        }
+    ]
+}
+
+console.log(usuario.nombre)
+console.log(usuario.apellidos)
+console.log(usuario.email)
+
